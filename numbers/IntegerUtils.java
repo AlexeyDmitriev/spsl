7d2b32e0f822ca0521fd80a44e9922d0f811c991
Alexey Dmitriev <riadwaw@gmail.com>
2012-07-22 02:10:07 +0400

    Primes

--- numbers/IntegerUtils.java
+++ numbers/IntegerUtils.java
@@ -1,5 +1,9 @@
 package numbers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: riad
@@ -32,5 +36,35 @@ public class IntegerUtils {
 
     static public long modInverse(long number, long mod) {
         return power(number, mod - 2, 2);
+    }
+
+    static boolean isPrime(long number) {
+        for (int i = 2; i * i <= number; ++i) {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    static boolean[] getPrimesMap(int to) {
+        boolean[] result = new boolean[to + 1];
+        Arrays.fill(result, true);
+        result[0] = result[1] = false;
+        for (int i = 2; i <= to; ++i)
+            if (result[i])
+                if (i * 1L * i <= to)
+                    for (int j = i * i; j <= to; j += i)
+                        result[j] = false;
+        return result;
+    }
+
+    static List<Integer> getPrimes(int to) {
+        List<Integer> result = new ArrayList<Integer>();
+        boolean[] map = getPrimesMap(to);
+        for (int i = 2; i <= to; ++i) {
+            if (map[i])
+                result.add(i);
+        }
+        return result;
     }
 }
