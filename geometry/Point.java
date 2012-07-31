b44d6dd13b3abd379344f6b5e0204c1a65de0e5f
Alexey Dmitriev <riadwaw@gmail.com>
2012-07-31 10:28:59 +0400

    Point::rotate()

--- geometry/Point.java
+++ geometry/Point.java
@@ -66,4 +66,14 @@ public class Point {
         return "(" + x + ", " + y + ")";
     }
 
+    public Point rotate(double angle) {
+        double cos = Math.cos(angle);
+        double sin = Math.sin(angle);
+        return new Point(x * cos - y * sin, x * sin + y * cos);
+    }
+
+    public Point rotate(double angle, Point center) {
+        return center.add(this.subtract(center).rotate(angle));
+    }
+
 }
