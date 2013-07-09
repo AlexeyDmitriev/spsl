57523d6ba394660336e063a5b8a0394cc0c95b26
Alexey Dmitriev <riadwaw@gmail.com>
2013-07-09 07:13:29 +0400

    Make Counter using HashMap<Long>

--- collections/Counter.java
+++ collections/Counter.java
@@ -1,17 +1,17 @@
 package collections;
 
-import java.util.TreeMap;
+import java.util.HashMap;
 
-public class Counter<T> extends TreeMap<T, Integer> {
+public class Counter<T> extends HashMap<T, Long> {
     public void add(T key) {
         add(key, 1);
     }
 
-    public void add(T key, int value) {
+    public void add(T key, long value) {
         put(key, get(key) + value);
     }
 
-    public Integer get(Object key) {
+    public Long get(Object key) {
         return containsKey(key) ? super.get(key) : 0;
     }
 
