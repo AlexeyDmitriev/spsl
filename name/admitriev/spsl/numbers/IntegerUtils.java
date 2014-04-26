8ca7d464e2be3b4f740f9db64577d61c7edf8dad
Alexey Dmitriev <github@admitriev.name>
2014-04-26 04:47:42 +0400

    euler totient func

--- name/admitriev/spsl/numbers/IntegerUtils.java
+++ name/admitriev/spsl/numbers/IntegerUtils.java
@@ -180,4 +180,17 @@ public class IntegerUtils {
 			}
 		}
 	}
+
+    public static long phi(long n) {
+        long res = n;
+        for(int i = 2; i * i <= n; ++i) {
+            if(n % i == 0) {
+                res /= i;
+                res *= i - 1;
+                while (n % i == 0)
+                    n /= i;
+            }
+        }
+        return res;
+    }
 }
